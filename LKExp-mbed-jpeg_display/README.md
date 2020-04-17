b434336764afb94932f64d96f6dc0a1c7adfe49c
Yann Locatelli <yann.locatelli2@gmail.com>
2020-04-17 17:32:06 +0200

    Failed attempt to  use customized SDBlockDevice
    Adding SDIOBlockDevice files from git mentionned in README.md, they were
    adapted to the SD of Disco-769's BSP. No error at compilation but at
    execution (spy with debug). Error Value is 0x200023A8.

--- LKExp-mbed-jpeg_display/README.md
+++ LKExp-mbed-jpeg_display/README.md
@@ -10,7 +10,14 @@ Display a JPEG image on the LCD using Jpeg Codec, DMA2D, LTDC and DSI if necessa
 
 Mount SD (BSP) : [BD\_SD\_DISCO\_F769NI](https://os.mbed.com/users/kenjiArai/code/BD_SD_DISCO_F769NI/)
 
+Mount SD (SDBlockDevice customized) : [sdio-driver](https://github.com/JojoS62/Test-lvgl/tree/master/components/sdio-driver) **[Does not work]**
+
+
 ## Status
 
 In progress...
 Next step : Display an image in easiest way (from flash instead of SD?, a bitmap file instead of jpeg?)
+
+## Warning
+
+Do not use [SDBlockDevice](https://os.mbed.com/docs/mbed-os/v5.15/apis/sdblockdevice.html), it required an SD host with SPI interface. In case of Disco-769, it is a SDMMC interface.
--- LKExp-mbed-jpeg_display/src/main.cpp
+++ LKExp-mbed-jpeg_display/src/main.cpp
@@ -15,6 +15,11 @@
 #include "stm32f769i_discovery_sd.h"
 #endif
 
+#ifdef SD_V2
+/* SD_v2 part */
+#include "SDIOBlockDevice.h"
+#include "FATFileSystem.h"
+#endif
 
 /* Exported constants --------------------------------------------------------*/
 /* DEBUG part */
@@ -34,6 +39,12 @@ BSP_SD_CardInfo _current_card_info;
 uint8_t _sd_state;
 //PlatformMutex _mutex;
 #endif
+    
+#ifdef SD_V2
+/* SD_v2 part */
+SDIOBlockDevice bd;
+FATFileSystem fs("fs", &bd);
+#endif
 
 /* Private function prototypes -----------------------------------------------*/
 /* LCD part */
@@ -41,6 +52,8 @@ static void LCD_BriefDisplay(void);
 
 /* Private functions ---------------------------------------------------------*/
 int main(void) {
+    debug_if(DBG, "\n\nRESET\r\n\n");
+
     /* LCD part */
     //    BSP_LCD_Init();
     //    BSP_LCD_LayerDefaultInit(0, LCD_FRAME_BUFFER);
@@ -55,9 +68,16 @@ int main(void) {
     _sd_state = BSP_SD_Init();
     BSP_SD_GetCardInfo(&_current_card_info);
 #endif
+    
+#ifdef SD_V2
+    /* SD_v2 part */
+    //sd.init();
+    //fflush(stdout);
+    //int err = fs.mount(bd);
+    //debug_if(DBG, "%s\n", (err ? "Fail :(" : "OK"));
+#endif
 
     /* DEBUG part */
-    printf("\n\nRESET\r\n\n");
     while (true) {
         debug_if(DBG, "test debug\r\n");
         wait_us(1000000);
