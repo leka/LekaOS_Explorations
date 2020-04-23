1178079cc94dae717022fe781fb7c16aeb07dda4
Yann Locatelli <yann.locatelli2@gmail.com>
2020-04-23 02:13:23 +0200

    LCD display with event feature + cleaning
    * LCD display is now associated with event "Wake Up"
    * Old way to use "Wake Up" is now removed
    * main was reorganized, especially when text is displayed
    * #define STM32F769DISCO was renamed to a more accurate name
      (#define LCD_DISPLAY)
    * Both example of waiting (wait_us and sleep_for) were kept. However,
      avoid to use sleep_for when display on LCD screen

--- LKExp-mbed-x-nucleo-iks01a2/src/main.cpp
+++ LKExp-mbed-x-nucleo-iks01a2/src/main.cpp
@@ -1,9 +1,9 @@
-//# define STM32F769DISCO
+# define LCD_DISPLAY
  
 /* Includes */
 #include "mbed.h"
 #include "XNucleoIKS01A2.h"
-#ifdef STM32F769DISCO
+#ifdef LCD_DISPLAY
 #include "stm32f769i_discovery_lcd.h"
 #endif
  
@@ -19,7 +19,6 @@ static LSM6DSLSensor *acc_gyro = mems_expansion_board->acc_gyro;
 static LSM303AGRAccSensor *accelerometer = mems_expansion_board->accelerometer;
 
 DigitalOut myled(LED1);
-volatile int mems_event = 0;
 EventQueue queue(1 * EVENTS_EVENT_SIZE);
 Mutex stdio_mutex;
 
@@ -59,37 +58,45 @@ static char *print_double(char* str, double v, int decimalDigits=2)
   return str;
 }
 
-/* Interrupt 1 callback. */
-void int2_cb() {
-  mems_event = 1;
-}
-
 void shakeDisplay() {
-    stdio_mutex.lock();
-    printf("\r\nWake Up Detected!\r\n");
-    stdio_mutex.unlock();
-
-    wait_us(3000000);
-    myled = 0;
+    LSM6DSL_Event_Status_t status;
+    acc_gyro->get_event_status(&status);
+    if (status.WakeUpStatus) {
+        /* Led blinking. */
+        myled = 1;
+  
+        /* Output data. */
+#ifdef LCD_DISPLAY
+        BSP_LCD_DisplayStringAt(0, 450, (uint8_t*)"Wake Up Detected!", CENTER_MODE);
+#else
+        stdio_mutex.lock();
+        printf("Wake Up Detected!\r\n");
+        stdio_mutex.unlock();
+#endif
+        wait_us(3000000);
+//        ThisThread::sleep_for(3000);
+        myled = 0;
+#ifdef LCD_DISPLAY
+        BSP_LCD_DisplayStringAt(0, 450, (uint8_t*)"                         ", CENTER_MODE);
+#endif
+    }
 }
 
 void onDataReceived() {
     myled = 1;
     queue.call(&shakeDisplay);
 }
- 
+
+void LCD_show();
+void terminal_show();
+
 /* Simple main function */
 int main() {
-  uint8_t id;
-  float value1, value2;
-  char buffer1[32], buffer2[32];
-  int32_t axes[3];
-
   Thread eventThread;
   eventThread.start(callback(&queue, &EventQueue::dispatch_forever));
   
-  /* Attach callback to LSM6DSL INT1 */
-    acc_gyro->attach_int2_irq(&onDataReceived);
+  /* Attach callback to LSM6DSL INT2 */
+  acc_gyro->attach_int2_irq(&onDataReceived);
     
   /* Enable all sensors */
   hum_temp->enable();
@@ -103,6 +110,7 @@ int main() {
   
   printf("\r\n--- Starting new run ---\r\n");
  
+  uint8_t id;
   hum_temp->read_id(&id);
   printf("HTS221  humidity & temperature    = 0x%X\r\n", id);
   press_temp->read_id(&id);
@@ -114,7 +122,7 @@ int main() {
   acc_gyro->read_id(&id);
   printf("LSM6DSL accelerometer & gyroscope = 0x%X\r\n", id);
 
-#ifdef STM32F769DISCO
+#ifdef LCD_DISPLAY
   BSP_LCD_Init();
   BSP_LCD_LayerDefaultInit(0, LCD_FB_START_ADDRESS);
   BSP_LCD_SelectLayer(0);
@@ -122,95 +130,84 @@ int main() {
   
   BSP_LCD_SetFont(&Font24);
   BSP_LCD_DisplayStringAt(0, 0, (uint8_t*)"IMU example", CENTER_MODE);
-  uint8_t lcd_string[100];
   BSP_LCD_SetFont(&Font20);
 #endif
- 
-  while(1) {
-      stdio_mutex.lock();
-#ifndef STM32F769DISCO
-    printf("\r\n");
+
+#ifdef LCD_DISPLAY
+    while(1) {
+        LCD_show();
+        wait_us(100000);
+    }
+#else
+    while(1) {
+        terminal_show();
+        ThisThread::sleep_for(100);
+    }
 #endif
+}
+
+void LCD_show() {
+    float value1, value2;
+    char buffer1[32], buffer2[32];
+    int32_t axes[3];
+    uint8_t lcd_string[100];
     
     hum_temp->get_temperature(&value1);
     hum_temp->get_humidity(&value2);
-#ifndef STM32F769DISCO
-    printf("HTS221: [temp] %7s C,   [hum] %s%%\r\n", print_double(buffer1, value1), print_double(buffer2, value2));
-#else
     sprintf((char*)lcd_string,"HTS221: [temp] %7s C,   [hum] %s%%               ", print_double(buffer1, value1), print_double(buffer2, value2));
     BSP_LCD_DisplayStringAt(0, 100, (uint8_t*)lcd_string, LEFT_MODE);
-#endif
 
     press_temp->get_temperature(&value1);
     press_temp->get_pressure(&value2);
-#ifndef STM32F769DISCO
-    printf("LPS22HB: [temp] %7s C, [press] %s mbar\r\n", print_double(buffer1, value1), print_double(buffer2, value2));
-    printf("---\r\n");
-#else
     sprintf((char*)lcd_string,"LPS22HB: [temp] %7s C, [press] %s mbar               ", print_double(buffer1, value1), print_double(buffer2, value2));
     BSP_LCD_DisplayStringAt(0, 150, (uint8_t*)lcd_string, LEFT_MODE);
-#endif
     
     magnetometer->get_m_axes(axes);
-#ifndef STM32F769DISCO
-    printf("LSM303AGR [mag/mgauss]:  %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
-#else
     sprintf((char*)lcd_string,"LSM303AGR [mag/mgauss]:  %6ld, %6ld, %6ld               ", axes[0], axes[1], axes[2]);
     BSP_LCD_DisplayStringAt(0, 200, (uint8_t*)lcd_string, LEFT_MODE);
-#endif
 
     accelerometer->get_x_axes(axes);
-#ifndef STM32F769DISCO
-    printf("LSM303AGR [acc/mg]:  %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
-#else
     sprintf((char*)lcd_string,"LSM303AGR [acc/mg]:  %6ld, %6ld, %6ld               ", axes[0], axes[1], axes[2]);
     BSP_LCD_DisplayStringAt(0, 250, (uint8_t*)lcd_string, LEFT_MODE);
-#endif
 
     acc_gyro->get_x_axes(axes);
-#ifndef STM32F769DISCO
-    printf("LSM6DSL [acc/mg]:      %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
-#else
     sprintf((char*)lcd_string,"LSM6DSL [acc/mg]: %6ld, %6ld, %6ld               ", axes[0], axes[1], axes[2]);
     BSP_LCD_DisplayStringAt(0, 300, (uint8_t*)lcd_string, LEFT_MODE);
-#endif
 
     acc_gyro->get_g_axes(axes);
-#ifndef STM32F769DISCO
-    printf("LSM6DSL [gyro/mdps]:   %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
-#else
     sprintf((char*)lcd_string,"LSM6DSL [gyro/mdps]: %6ld, %6ld, %6ld               ", axes[0], axes[1], axes[2]);
     BSP_LCD_DisplayStringAt(0, 350, (uint8_t*)lcd_string, LEFT_MODE);
-#endif
- 
-    if (mems_event) {
-    mems_event = 0;
-    LSM6DSL_Event_Status_t status;
-    acc_gyro->get_event_status(&status);
-    if (status.WakeUpStatus) {
-        /* Led blinking. */
-        myled = 1;
-  
-        /* Output data. */
-#ifndef STM32F769DISCO
-        printf("Wake Up Detected!\r\n");
-#else
-        BSP_LCD_DisplayStringAt(0, 450, (uint8_t*)"Wake Up Detected!", CENTER_MODE);
-#endif
-//        wait_us(3000000);
-        ThisThread::sleep_for(3000);
-        myled = 0;
-#ifdef STM32F769DISCO
-        BSP_LCD_DisplayStringAt(0, 450, (uint8_t*)"                         ", CENTER_MODE);
-#endif
-    }
-    }
-#ifndef STM32F769DISCO
-    printf("---\r\n");
-#endif
-    stdio_mutex.unlock();
+}
 
-//    wait_us(100000);
-    ThisThread::sleep_for(100);
-  }
+void terminal_show() {
+    float value1, value2;
+    char buffer1[32], buffer2[32];
+    int32_t axes[3];
+    
+    stdio_mutex.lock();
+    printf("\r\n");
+    
+    hum_temp->get_temperature(&value1);
+    hum_temp->get_humidity(&value2);
+    printf("HTS221: [temp] %7s C,   [hum] %s%%\r\n", print_double(buffer1, value1), print_double(buffer2, value2));
+
+    press_temp->get_temperature(&value1);
+    press_temp->get_pressure(&value2);
+    printf("LPS22HB: [temp] %7s C, [press] %s mbar\r\n", print_double(buffer1, value1), print_double(buffer2, value2));
+    printf("---\r\n");
+    
+    magnetometer->get_m_axes(axes);
+    printf("LSM303AGR [mag/mgauss]:  %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
+
+    accelerometer->get_x_axes(axes);
+    printf("LSM303AGR [acc/mg]:  %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
+
+    acc_gyro->get_x_axes(axes);
+    printf("LSM6DSL [acc/mg]:      %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
+
+    acc_gyro->get_g_axes(axes);
+    printf("LSM6DSL [gyro/mdps]:   %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
+
+    printf("---\r\n");
+    stdio_mutex.unlock();
 }
