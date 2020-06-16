bd03f0ce9d7fa6691c4c2b439f7f760e0e9bab2d
SamHadjes <samuel.hadjes@gmail.com>
2020-06-16 18:44:12 +0200

    Working on interrupts
    - INT1 is apparently working, we solved the I3C startup problem with a DigitalInOut on the A5 pin.
     Once the sensor has booted and I3C has been disabled, the pin can be set as input to allow interrupts
    
    - INT2 still not working, we probably missed a configuration register, will check the documentation.
    
    - Code has been cleaned up a little bit, we will do a Doxygen documentation soon because it isn't up to date
    
    - Also removed BUILD/ folder as it has no reason to be on distant repository

--- LKExp-Sensors-LSM6DSOX-Mbed_Integration/src/main.cpp
+++ LKExp-Sensors-LSM6DSOX-Mbed_Integration/src/main.cpp
@@ -15,15 +15,12 @@ using namespace MachineLearningCore;
 
 #define PIN_I2C1_SDA (D14)
 #define PIN_I2C1_SCL (D15)
-//#define PIN_LSM6DSOX_INT1 (A5)
-#define PIN_LSM6DSOX_INT1 NC
+#define PIN_LSM6DSOX_INT1 (A5)
 #define PIN_LSM6DSOX_INT2 (A4)
 
 //##################################################################################################
 // Prototypes
 //##################################################################################################
-
-void ISR_btn();
 
 void ISR_INT1();
 void ISR_INT2();
@@ -31,11 +28,11 @@ void ISR_INT2();
 void mlc_interrupt_on_1();
 void mlc_interrupt_on_2();
 
+void printState6D();
+
 //##################################################################################################
 // Global Variables
 //##################################################################################################
-
-DigitalOut INT_1_LSM6DSOX(A5, 0);	// This line fix the use of LSM6DSOX on X-NUCLEO_IKS01A2
 
 I2C i2c1(PIN_I2C1_SDA, PIN_I2C1_SCL);
 Communication::LSM6DSOX_I2C lsm6dsox_i2c(i2c1);
@@ -45,61 +42,42 @@ Component::LSM6DSOX_Gyroscope lsm6dsox_gyroscope_component(lsm6dsox_i2c, PIN_LSM
 MachineLearningCore::LSM6DSOX_MachineLearningCore lsm6dsox_mlc(lsm6dsox_i2c, PIN_LSM6DSOX_INT1, 
 																			PIN_LSM6DSOX_INT2);
 
+
+//The A5 (INT1) pin has to be set to low at startup in order for the LSM6DSOX to mantain I2C connectivity
+//Once the sensor has had time to startup, it can receive the "disable I3C" command, thus forcing I2C mode
+//Once I3C has been deactivated, INT1 pin has to be set to input for the interrupts to work properly
+DigitalInOut INT_1_LSM6DSOX(PIN_LSM6DSOX_INT1, PIN_OUTPUT, PullNone, 0);
+																	
+
 // Flags for interrupts
 bool int1Flag = false;
 bool int2Flag = false;
-
-bool btnFlag = false;
 
 //##################################################################################################
 // Main
 //##################################################################################################
 
 int main(void) {
-
 	//-------------------------------------------------------------------------------------
 	// Initial setup
 
-	/* Wait sensor boot time */
-	ThisThread::sleep_for(10ms);
-	printf("\nStarting a new run!\r\n\n");
+	ThisThread::sleep_for(10ms);	//waiting for sensor startup
+	lsm6dsox_mlc.disableI3C();		//disabling I3C on sensor
+	INT_1_LSM6DSOX.input();			//setting up INT1 pin as input (for interrupts to work)
 
-	/* Disable I3C in order to use interrupts */
-	lsm6dsox_mlc.disableI3C();
-
-	//lsm6dsox_mlc.setInterrupt1Pin((A5));
-
-
-	// Get sensor ID
-	uint8_t id;
-	lsm6dsox_mlc.getID(id);
-	if (id != LSM6DSOX_ID) {
-		printf("No id got from sensor\n\n");
-		while (1)
-			;
-	}
+	//First message to serial
+	printf("\nStarting a new run!\n\n");
 
 	// Inits
-	printf("Status init: %d\n", lsm6dsox_mlc.init());
-	lsm6dsox_accelerometer_component.init();
-	lsm6dsox_gyroscope_component.init();
-
-	
-
-	// Set interrupt pins
-	// delete(&INT_1_LSM6DSOX);
-	// DigitalIn INT_2_LSM6DSOX(A4);
-	// DigitalIn INT_1_LSM6DSOX(A5);
+	lsm6dsox_mlc.init();						//mlc
+	lsm6dsox_accelerometer_component.init();	//xl
+	lsm6dsox_gyroscope_component.init();		//gy
 
 	//-------------------------------------------------------------------------------------
-	// MLC data rate test (write and read)
+	// MLC data rate
 
-	// test of data rate
-	float odr = 12.0f;
+	float odr = 26.0f;
 	lsm6dsox_mlc.setDataRate(odr);
-	printf("Status data rate get: %d\n", lsm6dsox_mlc.getDataRate(odr));
-
-	printf("The data rate has been set to: %d (value was multiplied by 1000)\n", (int)(odr * 1000));
 
 	//-------------------------------------------------------------------------------------
 	// Writing a decision tree to the MLC
@@ -111,29 +89,25 @@ int main(void) {
 
 	//-------------------------------------------------------------------------------------
 	// Setting up the xl and the gyro to work properly
-
-	/* At this point the device is ready to run but if you need you can also
-	 * interact with the device but taking in account the MLC configuration.
-	 *
-	 * For more information about Machine Learning Core tool please refer
-	 * to AN5259 "LSM6DSOX: Machine Learning Core".
-	 */
-
-	/* Turn off Sensors */
+	// TODO
+	// we have to work out where to do these verifications
+	
+	// Turn off Sensors
 	lsm6dsox_accelerometer_component.setPowerMode(Component::PowerMode::OFF);
 	lsm6dsox_gyroscope_component.setPowerMode(Component::PowerMode::OFF);
 
-	/* Enable Block Data Update */
+	// Enable Block Data Update
+	// TODO
+	// this still has to be clarified
 	lsm6dsox_block_data_update_set(lsm6dsox_mlc.TMP_getIoFunc(), PROPERTY_ENABLE);
 
-	/* Set full scale */
+	// Set full scale
 	lsm6dsox_accelerometer_component.setRange(Component::AccelerometerRange::_4G);
 	lsm6dsox_gyroscope_component.setRange(Component::GyroscopeRange::_2000DPS);
 
-	/* Set Output Data Rate.
-	 * Selected data rate have to be equal or greater with respect
-	 * with MLC data rate.
-	 */
+	// Set Output Data Rate.
+	// Selected data rate have to be equal or greater with respect
+	// with MLC data rate.
 	lsm6dsox_gyroscope_component.setPowerMode(Component::PowerMode::OFF);
 
 	lsm6dsox_accelerometer_component.setPowerMode(Component::PowerMode::NORMAL);
@@ -142,37 +116,32 @@ int main(void) {
 	//-------------------------------------------------------------------------------------
 	// Setting up interrupt for MLC
 
-	/* Configure interrupt pin mode notification */
-	lsm6dsox_int_notification_set(lsm6dsox_mlc.TMP_getIoFunc(), LSM6DSOX_BASE_PULSED_EMB_LATCHED);
+	// Configure interrupt pin mode notification
+	// TODO
+	// this still has to be clarified
+	lsm6dsox_int_notification_set(lsm6dsox_mlc.TMP_getIoFunc(), LSM6DSOX_ALL_INT_LATCHED); 
+	//   LSM6DSOX_ALL_INT_PULSED            = 0
+	//   LSM6DSOX_BASE_LATCHED_EMB_PULSED   = 1
+	//   LSM6DSOX_BASE_PULSED_EMB_LATCHED   = 2
+	//   LSM6DSOX_ALL_INT_LATCHED           = 3
 	
-	/* Route signals on interrupt pin 1 and 2 */
+	// Route signals on interrupt
 	lsm6dsox_mlc.enableTreeInterrupt(MachineLearningCoreTree::_TREE_1,
 									 TreeInterruptNum::_INT1);
 
-
+	// verifying interrupt was set correctly
 	lsm6dsox_pin_int1_route_t   pin_int1_route;
 	lsm6dsox_pin_int1_route_get(lsm6dsox_mlc.TMP_getIoFunc(), &pin_int1_route);
-	printf("Interrupt 1 enabled mlc1: %d\n", pin_int1_route.mlc1);
-
-
+	printf("Interrupt 1 route for mlc1: %d\n", pin_int1_route.mlc1);
 
 	//-------------------------------------------------------------------------------------
 	// Testing interrupts
-	//DigitalIn INT_1_LSM6DSOX(A5);
-	InterruptIn int1(A5);
-	int1.rise(ISR_INT1);
-	// lsm6dsox_mlc.setInterrupt1Pin(A5);
-	// lsm6dsox_mlc.attachInterrupt1(ISR_INT1);
-
-	//lsm6dsox_mlc.attachInterrupt1(ISR_INT1);
-	//lsm6dsox_mlc.attachInterrupt2(ISR_INT2);
+	lsm6dsox_mlc.attachInterrupt1(ISR_INT1);
+	lsm6dsox_mlc.attachInterrupt2(ISR_INT2);
 	
-	InterruptIn btn(BUTTON1);
-	btn.rise(ISR_btn);
-
-	/* Main loop */
+	//-------------------------------------------------------------------------------------
+	// Main loop
 	while (1) {
-
 		if(int1Flag)
 		{
 			lsm6dsox_all_sources_t status;
@@ -180,27 +149,21 @@ int main(void) {
 			if (status.mlc1)
 			{
 				mlc_interrupt_on_1();
-				printf("Interrupt on INT1\n\n");
 			}
 			int1Flag = false;
 		}
-		// lsm6dsox_all_sources_t status;
-		// lsm6dsox_all_sources_get(lsm6dsox_mlc.TMP_getIoFunc(), &status);
-		// if(status.mlc1)
-		// {
-		// 	printf("Interrupt detected from mlc1\n");
-		// 	uint8_t intStatus;
-		// 	lsm6dsox_mlc.readInterrupt2(intStatus);
-		// 	printf("pin int 2 status: %d\n", intStatus);
-		// }
-
-		// uint8_t intStatus;
-		// lsm6dsox_mlc.readInterrupt2(intStatus);
-		// printf("pin int 2 status: %d\n", intStatus);
-
+		else if(int2Flag)
+		{
+			lsm6dsox_all_sources_t status;
+			lsm6dsox_all_sources_get(lsm6dsox_mlc.TMP_getIoFunc(), &status);
+			if (status.mlc1)
+			{
+				mlc_interrupt_on_2();
+			}
+			int1Flag = false;
+		}
 		ThisThread::sleep_for(1s);
 	}
-
 	return 0;
 }
 
@@ -208,64 +171,33 @@ int main(void) {
 // Functions
 //##################################################################################################
 
-void ISR_btn() { btnFlag = true; }
-
-// ISR for INT1 (with flags)
+// ISR for INT1
 void ISR_INT1() {
 	int1Flag = true;
 }
 
-// ISR for INT2 (with flags)
+// ISR for INT2
 void ISR_INT2() {
-	lsm6dsox_all_sources_t status;
-	lsm6dsox_all_sources_get(lsm6dsox_mlc.TMP_getIoFunc(), &status);
-	if (status.mlc1) int2Flag = true;
+	int2Flag = true;
 }
 
+//function called if int1Flag is set
 void mlc_interrupt_on_1() {
-
-	MachineLearningCoreData values;
-	lsm6dsox_mlc.getData(values.data);
-
-	// MLC0_SRC register values for 6D position
-	// 0 = None
-	// 1 = X-axis pointing up
-	// 2 = X-axis pointing down
-	// 3 = Y-axis pointing up
-	// 4 = Y-axis pointing down
-	// 5 = Z-axis pointing up
-	// 6 = Z-axis pointing down
-
-	switch (values.mlcTreeVal.tree1) {
-		case 1:
-			printf("X axis up\n");
-			break;
-		case 2:
-			printf("X axis down\n");
-			break;
-		case 3:
-			printf("Y axis up\n");
-			break;
-		case 4:
-			printf("Y axis down\n");
-			break;
-		case 5:
-			printf("Z axis up\n");
-			break;
-		case 6:
-			printf("Z axis down\n");
-			break;
-		default:
-			printf("None (data hasn't been updated by tree)\n");
-			break;
-	}
+	printf("Interrupt comming from: INT1\n");
+	printState6D();
 }
 
+//function called if int2Flag is set
 void mlc_interrupt_on_2() {
+	printf("Interrupt comming from: INT2\n");
+	printState6D();
+}
 
+//Printing state of 6D position
+void printState6D()
+{
 	MachineLearningCoreData values;
 	lsm6dsox_mlc.getData(values.data);
-
 	// MLC0_SRC register values for 6D position
 	// 0 = None
 	// 1 = X-axis pointing up
@@ -276,6 +208,9 @@ void mlc_interrupt_on_2() {
 	// 6 = Z-axis pointing down
 
 	switch (values.mlcTreeVal.tree1) {
+		case 0:
+			printf("None (Uncertain position)\n");
+			break;
 		case 1:
 			printf("X axis up\n");
 			break;
@@ -293,9 +228,6 @@ void mlc_interrupt_on_2() {
 			break;
 		case 6:
 			printf("Z axis down\n");
-			break;
-		default:
-			printf("None (data hasn't been updated by tree)\n");
 			break;
 	}
 }
