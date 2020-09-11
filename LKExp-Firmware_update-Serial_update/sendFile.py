1310708fa181f9892c807386ee3dac8e885c3b90
Yann Locatelli <yann.locatelli2@gmail.com>
2020-09-11 12:42:14 +0200

    Step 2 - Send new application
    * Now possible to send an application in .hex through Serial.
    * Python script read and send line by line the .hex file and wait an ACK char
    from receiver after delivery.
    * Bootloader erase prior to write program.
    * Program received used hex file structure, included the checksum.
    * Offset changed due to growing size of bootloader.
    * LED added in Application to have visual confirmation.

---
+++ LKExp-Firmware_update-Serial_update/sendFile.py
@@ -0,0 +1,51 @@
+import serial
+import binascii
+import time
+
+print("Starting script to send hex file\n")
+
+serial = serial.Serial(port='/dev/tty.usbmodem14203', baudrate = 115200)
+time.sleep(1)
+
+len_data = serial.in_waiting
+s = serial.read(len_data)
+# print (s)
+
+# print(serial.isOpen())
+
+# file = open('test.hex','r')
+# file = open('APPLICATION_application.hex','r')
+file = open('APPLICATION/BUILD/DISCO_F769NI/GCC_ARM/APPLICATION_application.hex','r')
+code = 0
+
+text = file.readline()[1:-1]
+line = binascii.unhexlify(text)
+serial.write(line)
+
+s = serial.read(1)
+while s != b'0':
+    s = serial.read(1)
+    # print (s)
+    time.sleep(0.01)
+time.sleep(1)
+
+while code != 1 :
+
+    text = file.readline()[1:-1]
+    line = binascii.unhexlify(text)
+
+    serial.write(line)
+    code = line[3]
+
+    s = serial.read(1) # Wait ACK 
+    # print (s)
+    if s == b'1' :
+        break
+
+if s == b'1' :
+    print("Error occurs, try again")
+else:
+    print("Finished send")
+serial.close()
+
+file.close()
