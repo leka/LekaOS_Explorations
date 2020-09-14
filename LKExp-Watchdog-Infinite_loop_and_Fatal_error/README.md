ba1f0f57d089cb4f805f9d70a2698aa7d438e16b
Yann Locatelli <yann.locatelli2@gmail.com>
2020-09-14 14:02:53 +0200

    Add Infinite loop/Error + EventQueue/Ticker
    * Use Infinite loop and Error as "freeze" status
    * They can be selected with the USER button
    * To refresh watchdog, EventQueue or Ticker can be choosed
    * For now, only Ticker seems to work in case of error, not in Infinite
    loop

--- LKExp-Watchdog-Infinite_loop_and_Fatal_error/README.md
+++ LKExp-Watchdog-Infinite_loop_and_Fatal_error/README.md
@@ -7,3 +7,15 @@ Final goal is to prevent freeze of the robot.
 ⚠️ Last version of mbed-os (commit : ad973f2) remove .hex file generation.
 The loss is probabily caused by this commit : https://github.com/ARMmbed/mbed-os/commit/5d245ad5537bc8c54795a87549258a6c22df5087
 mbed_app.json was adapted to generate .hex file instead of .bin file.
+
+## Current issue
+
+Add the two types of "freeze" : infinite loop and fatal error. They can be selected with the button USER on DiscoF769.
+Also, two kinds of tools can be used to refresh the watchdog : EventQueue and Ticker.
+
+## Results
+
+When use Ticker and Infinite loop : watchdog is never triggered.
+When use Ticker and Fatal error : watchdog reboot the board.
+When use EventQueue and Infinite loop : Program seems to be stucked in `dispatch()` function.
+When use EventQueue and Fatal error : Program seems to be stucked in `dispatch()` function.
--- LKExp-Watchdog-Infinite_loop_and_Fatal_error/src/main.cpp
+++ LKExp-Watchdog-Infinite_loop_and_Fatal_error/src/main.cpp
@@ -1,36 +1,64 @@
-/*
- * Copyright (c) 2020 Arm Limited and affiliates.
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
+
+#define USE_EVENTQUEUE 0
 
 const uint32_t TIMEOUT_MS = 5000;
 InterruptIn button(BUTTON1);
-volatile int countdown = 9;
+bool button_pressed = false;
 
-void trigger()
+#if USE_EVENTQUEUE
+EventQueue queue;
+#else
+Ticker kicker;
+#endif
+
+void pushed()
 {
-    Watchdog::get_instance().kick();
-    countdown = 9;
+	button_pressed = true;
+}
+
+void released()
+{
+	button_pressed = false;
+}
+
+void watchdogKick()
+{
+	Watchdog::get_instance().kick();
 }
 
 int main()
 {
-    printf("\r\nTarget started.\r\n");
+	printf("\r\n\r\nStarting a new run.\r\n");
 
-    Watchdog &watchdog = Watchdog::get_instance();
-    watchdog.start(TIMEOUT_MS);
-    button.rise(&trigger);
+	button.rise(&pushed);
+	button.fall(&released);
+	printf("Press BUTTON1 to switch between Infinite loop (button released) and Fatal error (button pressed)\n");
+	printf("Wait 5s to make your choice...\n");
 
-    uint32_t watchdog_timeout = watchdog.get_timeout();
-    printf("Watchdog initialized to %lu ms.\r\n", watchdog_timeout);
-    printf("Press BUTTON1 at least once every %lu ms to kick the "
-           "watchdog and prevent system reset.\r\n", watchdog_timeout);
+	ThisThread::sleep_for(5s);
 
-    while (1) {
-        printf("\r%3i", countdown--);
-        fflush(stdout);
-        ThisThread::sleep_for(TIMEOUT_MS / 10);
-    }
+	printf("You choosed ");
+	if (button_pressed) {
+		printf("Fatal error\n");
+	} else {
+		printf("Infinite loop\n");
+	}
+	printf("\r\nSet watchdog timeout to %lums\r\n", TIMEOUT_MS);
+	Watchdog &watchdog = Watchdog::get_instance();
+	watchdog.start(TIMEOUT_MS);
+
+#if USE_EVENTQUEUE
+	queue.call_every(1, &watchdogKick);
+	queue.dispatch();
+#else
+	kicker.attach_us(watchdogKick, 1000);
+#endif
+
+	if (button_pressed) {
+		error("This is a fatal error\n");
+	} else {
+		while (1) { printf("To Infinity and Beyond!! "); }
+	}
+	return 0;
 }
